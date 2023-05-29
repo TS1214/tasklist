81c87c745f766893b07190adfb91f5dc41fdb779
shiori.takano <2023dounyukenshu.046@gmail.com>
2023-05-29 15:43:29 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,28 +1,27 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import utils.DButil;import java.util.List;
-import javax.persistence.EntityManager;
 import models.Task;
-import javax.servlet.RequestDispatcher;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
     }
 
@@ -31,15 +30,16 @@ public class IndexServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em=DButil.createEntityManager();
+        //CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-        List<Task>tasks=em.createNamedQuery("getAllTasks",Task.class).getResultList();
+        //おまじないとしてのインスタンスを作成
+        request.setAttribute("task", new Task());
 
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
+     }
 
-       request.setAttribute("tasks", tasks);
-
-       RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
-       rd.forward(request, response);
     }
-}
+
+
